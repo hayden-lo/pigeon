a77b27ede3c1c24a6e00357071788c14a8c92d0c
laozhuonan <laozhuonan@xmiles.cn>
2024-09-25 11:13:52 +0800

    add config

--- main.go
+++ main.go
@@ -11,20 +11,13 @@ import (
 func main() {
 	router := gin.Default()
 
-	// // 读取配置
-	// config, err := utils.LoadConfig("config.yaml")
-	// if err != nil {
-	// 	log.Fatalf("读取配置文件失败: %v", err)
-	// }
-
 	// 设置路由
 	controllers.GetJokeByPage(router)
 	controllers.RecordUserAct(router)
 
 	// 启动服务器
 	port := fmt.Sprintf(":%d", utils.GlobalConfig.Server.Port)
-	fmt.Print(port)
-	// if err := router.Run(port); err != nil {
-	// 	panic(err)
-	// }
+	if err := router.Run(port); err != nil {
+		panic(err)
+	}
 }
