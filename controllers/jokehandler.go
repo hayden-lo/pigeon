fbe0707ee36c934a0f4039d7f2289c850e2b10f6
laozhuonan <laozhuonan@xmiles.cn>
2024-09-30 11:32:57 +0800

    add code and message

--- controllers/jokehandler.go
+++ controllers/jokehandler.go
@@ -17,17 +17,17 @@ func GetJokeByPage(router *gin.Engine) {
 	router.POST("/getJokeByPage", func(c *gin.Context) {
 		var data JokeReq
 		if err := c.ShouldBindJSON(&data); err != nil {
-			c.JSON(400, gin.H{"error": "Invalid JSON data"})
+			c.JSON(400, gin.H{"code": 101, "message": "Invalid JSON data"})
 			return
 		}
 
 		jokes, err := dao.GetJokeByPage(data.DeviceId, data.Page, data.PageSize)
 		if err != nil {
-			c.JSON(500, gin.H{"error": "Error fetching jokes"})
+			c.JSON(500, gin.H{"error": 201, "message": "Error fetching jokes"})
 			return
 		}
 
-		c.JSON(200, jokes)
+		c.JSON(200, gin.H{"code": 200, "message": "Success", "data": jokes})
 	})
 }
 
@@ -42,22 +42,22 @@ func RecordUserAct(router *gin.Engine) {
 	router.POST("/recordUserAct", func(c *gin.Context) {
 		var data UserActReq
 		if err := c.ShouldBindJSON(&data); err != nil {
-			c.JSON(400, gin.H{"error": "Invalid JSON data"})
+			c.JSON(400, gin.H{"code": 101, "message": "Invalid JSON data"})
 			return
 		}
 
 		if !(data.ActType == "like" || data.ActType == "show") {
-			c.JSON(400, gin.H{"error": "Wrong actType value"})
+			c.JSON(400, gin.H{"code": 102, "message": "Wrong actType value"})
 			return
 		}
 
 		err := dao.InsertUserAct(data.DeviceId, data.JokeId, data.ActType)
 		if err != nil {
-			c.JSON(500, gin.H{"error": "Error inserting record"})
+			c.JSON(500, gin.H{"code": 301, "message": "Error inserting record"})
 			return
 		}
 
-		c.JSON(200, gin.H{"status": "success", "message": "操作成功"})
+		c.JSON(200, gin.H{"code": 200, "message": "Success"})
 	})
 }
 
@@ -71,16 +71,16 @@ func GetUserShowHistory(router *gin.Engine) {
 	router.POST("/getUserShowHistory", func(c *gin.Context) {
 		var data UserHistoryReq
 		if err := c.ShouldBindJSON(&data); err != nil {
-			c.JSON(400, gin.H{"error": "Invalid JSON data"})
+			c.JSON(400, gin.H{"code": 101, "message": "Invalid JSON data"})
 			return
 		}
 
 		jokes, err := dao.GetUserShowHistory(data.DeviceId)
 		if err != nil {
-			c.JSON(500, gin.H{"error": "Error fetching user show history"})
+			c.JSON(500, gin.H{"code": 201, "message": "Error fetching user show history"})
 			return
 		}
 
-		c.JSON(200, jokes)
+		c.JSON(200, gin.H{"code": 200, "message": "Success", "data": jokes})
 	})
 }
