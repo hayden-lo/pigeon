40c68ccee88f4e54ed1b81144c225ddcf81377fe
hayden <haydenlao@buffolio.com>
2025-02-18 00:51:40 +0800

    .

--- entity/joke.go
+++ entity/joke.go
@@ -1,6 +1,10 @@
 package entity
 
 type Joke struct {
-	JokeId  string `json:"jokeId"`
-	Content string `json:"content"`
+	JokeId       string `json:"jokeId"`
+	Content      string `json:"content"`
+	Category     string `json:"category"`
+	JokeSetup    string `json:"jokeSetup"`
+	JokeDelivery string `json:"jokeDelivery"`
+	Language     string `json:"language"`
 }
